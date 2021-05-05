110e5e489d104901b66ee4a28c66a7fdd3539699
Dibimo <diogobissolimoreno@gmail.com>
2021-05-05 09:24:44 -0300

    Componente para Painel rotativo adicionado

---
+++ src/components/PainelRotativo/PainelRotativo.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import AwesomeSlider from "react-awesome-slider";
+import "react-awesome-slider/dist/styles.css";
+
+class PainelRotativo extends Component {
+
+  constructor(props){
+    super(props)
+    this.nomesImagens = [];
+    this.diretorio = "";
+    this.style= {};
+  }
+
+
+  
+
+  render() {
+    console.log(this.props.diretorio);
+    return (
+      <div>
+        <AwesomeSlider style={this.props.style}>
+          {this.props.nomesImagens.map((image) => (
+            <div
+              data-src={
+                `${this.props.diretorio + image.replace("./", "")}`
+              }
+            ></div>
+          ))}
+        </AwesomeSlider>
+      </div>
+    );
+  }
+}
+
+export default PainelRotativo;
