5b68d6a4f31eaf844af67c13137f14ed24074f1f
Dibimo <diogobissolimoreno@gmail.com>
2021-05-05 09:24:02 -0300

    Barra de navegação e Painel de imagens adicionados

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import BarraNavegacao from './../../components/BarraNavegacao/BarraNavegacao'
+import PainelRotativo from './../../components/PainelRotativo/PainelRotativo'
 import "./estilo.css"
+var listaImagens = [];
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                <BarraNavegacao></BarraNavegacao>
-            </div>
-        );
-    }
+  importAll(r) {
+    return r.keys();
+  }
+  
+  componentWillMount() {
+    listaImagens = this.importAll(
+      require.context(`./../../../public/imagens/imagensHome/`, false, /\.(png|jpe?g|svg)$/)
+    );
+  }
+  render() {
+    return (
+      <div>
+        <BarraNavegacao></BarraNavegacao>
+        <PainelRotativo
+        nomesImagens = {listaImagens}
+        diretorio = {'/imagens/imagensHome/'}
+        style={{ height: 396, width: 1280 }}
+        ></PainelRotativo>
+      </div>
+    );
+  }
 }
 
 export default Home;
