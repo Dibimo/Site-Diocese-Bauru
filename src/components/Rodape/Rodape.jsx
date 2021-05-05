023dcb8a504c0fb9da8bd8a1cd22d072157d0aa0
Dibimo <diogobissolimoreno@gmail.com>
2021-05-05 09:45:23 -0300

    Componente Rodape adicionado

---
+++ src/components/Rodape/Rodape.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import "./estilo.css"
+
+class Rodape extends Component {
+  
+  render() {
+    return (
+      <div>
+        <div>BISPADO DE BAURU - CÚRIA DIOCESANA</div>
+        <div>FONE: (14) 3879-8706</div>
+        <div>E-mail para contato: BISPADOBAURU@BISPADOBAURU.ORG.BR</div>
+      </div>
+    );
+  }
+}
+
+export default Rodape;
