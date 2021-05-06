a2971547a84139f7118331cad550e150aed77bb2
Dibimo <diogobissolimoreno@gmail.com>
2021-05-06 12:28:07 -0300

    Link para página de Administração implementado

--- src/components/BarraNavegacao/BarraNavegacao.jsx
+++ src/components/BarraNavegacao/BarraNavegacao.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import { Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import "./estilo.css"
 
 
@@ -14,7 +15,8 @@ const BarraNavegacao = (props) => {
             <div className="navbar-dark bg-danger border border-danger rounded d-flex flex-column">
                 <div className="p-2">
                     <Navbar className="bg-danger rounded" light expand="row">
-                    <Col><NavbarBrand href="/adm" className="d-flex flex-row float-left">Administração</NavbarBrand></Col>
+                    <Link to="/"><Col><NavbarBrand className="d-flex flex-row float-left">Home</NavbarBrand></Col></Link>
+                    <Link to="/administracao"><Col><NavbarBrand className="d-flex flex-row float-left">Administração</NavbarBrand></Col></Link>
                     <Col><NavbarBrand href="/paroquia" className="d-flex flex-row float-left">Paróquias</NavbarBrand></Col>
                     <Col><NavbarBrand href="/contato" className="d-flex flex-row float-left">Contato</NavbarBrand></Col>
                     <Col><NavItem href="/" className="d-flex flex-row float-rigth"><input type="text" placeholder="Pesquisar..."></input></NavItem></Col>
