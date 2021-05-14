3d5640ece019010a5907c6a6da8f3782197f1017
Dibimo <diogobissolimoreno@gmail.com>
2021-05-14 12:24:06 -0300

    Página de programção iniciada

---
+++ src/pages/Programacao/Programacao.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import BarraNavegacao from "./../../components/BarraNavegacao/BarraNavegacao";
+import Rodape from "./../../components/Rodape/Rodape";
+import Container from "@material-ui/core/Container";
+
+class Programacao extends Component {
+  
+  render() {
+    return (
+      <Container
+        style={{ backgroundColor: "#F3EEE8" }}
+        border="1px"
+        border-radius="5px"
+        sm="maxWidth"
+      >
+        <BarraNavegacao></BarraNavegacao>    
+        <Rodape></Rodape>
+      </Container>
+    );
+  }
+}
+
+export default Programacao;
