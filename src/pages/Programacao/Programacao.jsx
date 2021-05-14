649ff270d98c71c98454db1f167367fa381f192d
Dibimo <diogobissolimoreno@gmail.com>
2021-05-14 12:58:52 -0300

    Timeline de tweets adicionada

--- src/pages/Programacao/Programacao.jsx
+++ src/pages/Programacao/Programacao.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import BarraNavegacao from "./../../components/BarraNavegacao/BarraNavegacao";
 import Rodape from "./../../components/Rodape/Rodape";
 import Container from "@material-ui/core/Container";
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+
 
 class Programacao extends Component {
   
@@ -13,7 +15,13 @@ class Programacao extends Component {
         border-radius="5px"
         sm="maxWidth"
       >
-        <BarraNavegacao></BarraNavegacao>    
+        <BarraNavegacao></BarraNavegacao>
+        <TwitterTimelineEmbed
+          sourceType="@Pontifex_pt"
+          screenName="Pontifex_pt"
+          options={{ height: 400, width: 400 }}
+        />
+        
         <Rodape></Rodape>
       </Container>
     );
