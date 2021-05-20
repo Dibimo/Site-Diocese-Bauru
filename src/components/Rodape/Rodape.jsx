b9b2a7a4f660e9667211bb22e285d10bca21aca8
Dibimo <diogobissolimoreno@gmail.com>
2021-05-20 11:49:35 -0300

    Classe tituraRodape adicionada

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -5,7 +5,7 @@ class Rodape extends Component {
   
   render() {
     return (
-      <div className="tintura" align="center">
+      <div className="tintura tinturaRodape" align="center">
         <div className="margem"></div>
         <div  font="bold"><h5>CENTRO DIOCESANO DE PASTOAL | FONE: (14) 3234-6908</h5></div>
         <div><h6><strong>HORÁRIO DE ATENDIMENTO</strong>: DE SEGUNDA À SEXTA, 8H - 12H E 13H - 17H30</h6></div>
