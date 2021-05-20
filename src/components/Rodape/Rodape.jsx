e1b5dc9f14ce4ba7fb7604a7d218165fd537854b
Dibimo <diogobissolimoreno@gmail.com>
2021-05-20 11:47:08 -0300

    Rodapé atualizado

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -6,9 +6,11 @@ class Rodape extends Component {
   render() {
     return (
       <div className="tintura" align="center">
-        <div  font="bold"><h4>BISPADO DE BAURU - CÚRIA DIOCESANA</h4></div>
-        <div><h5>FONE: (14) 3879-8706</h5></div>
-        <div><h5>E-mail para contato: BISPADOBAURU@BISPADOBAURU.ORG.BR</h5></div>
+        <div className="margem"></div>
+        <div  font="bold"><h5>CENTRO DIOCESANO DE PASTOAL | FONE: (14) 3234-6908</h5></div>
+        <div><h6><strong>HORÁRIO DE ATENDIMENTO</strong>: DE SEGUNDA À SEXTA, 8H - 12H E 13H - 17H30</h6></div>
+        <div><h6>RUA FERNANDO COSTA, 3-30 - JARDIM ESTORIL - BAURU (SP)</h6></div>
+        <div><h6>E-MAIL: BISPADOBAURU@BISPADOBAURU.ORG.BR</h6></div>
       </div>
     );
   }  
