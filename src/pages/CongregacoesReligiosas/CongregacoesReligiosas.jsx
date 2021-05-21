f0b5c2217b01ee348a5d1207be284a9e3ed2103b
Dibimo <diogobissolimoreno@gmail.com>
2021-05-21 16:40:45 -0300

    Cards de informações adicionados

--- src/pages/CongregacoesReligiosas/CongregacoesReligiosas.jsx
+++ src/pages/CongregacoesReligiosas/CongregacoesReligiosas.jsx
@@ -2,10 +2,37 @@ import React, { Component } from "react";
 import BarraNavegacao from "./../../components/BarraNavegacao/BarraNavegacao";
 import Rodape from "./../../components/Rodape/Rodape";
 import Titulo from "./../../components/Titulo/Titulo";
+import congregacoes from "./../../data/congregracoesReligiosas.json";
 import Container from "@material-ui/core/Container";
-
-
+import CardCongregacao from "./CardCongregacao/CardCongregacao.jsx";
+import './estilos.css';
 class CongregacoesReligiosas extends Component {
+
+  controiCards(congregacoes){
+    let congregacoesNomes = Object.keys(congregacoes);
+    let cards = [];
+    congregacoesNomes.map((congregacaoNome)=>{
+      cards.push(
+        <CardCongregacao congregacao={congregacoes[congregacaoNome]}></CardCongregacao>
+      );
+    });
+    return cards;
+  }
+  constroiParoquias(){
+    //Controi 
+    let paroquiasDivs = [];
+    congregacoes.paroquias.map((paroquia)=>{
+      paroquiasDivs.push(
+        <div>{paroquia}</div>
+      );
+    });
+    return (
+      <div className="paroquiasCongregacoesMasculinas conteudoCardCongregacao" style={{ border: "1px solid" }}>
+        Paróquias:
+        {paroquiasDivs}
+      </div>
+    );
+  }
   
   render() {
     return (
@@ -17,10 +44,24 @@ class CongregacoesReligiosas extends Component {
       >
         <BarraNavegacao></BarraNavegacao>
         <Titulo titulo="Congregações Religiosas"></Titulo>
-        <div align="center">
-          <button width="200px" height="80px">Masculinas</button>
-          <button width="200px" height="80px">Femininas</button>
+
+        <div className="congregacoesConteiner">
+          <section className="congregacoesMasculinas">
+            <button width="200px" height="80px">Masculinas</button>
+            {this.controiCards(congregacoes.masculinas)}
+            {this.constroiParoquias()}
+          </section>
+
+          <section className="congregacoesFemininas">
+            <button width="200px" height="80px">Femininas</button>
+            {this.controiCards(congregacoes.femininas)}
+          </section>
         </div>
+        <section className="outrosCongregacoesConteiner">
+          <div className="outrosCongregacoes">
+            {this.controiCards(congregacoes.outras)}
+          </div>
+        </section>
         <Rodape></Rodape>
       </Container>
     );
