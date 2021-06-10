e2b6cfae8f0930bcf3a4b5701982e1ffaeb2a689
Dibimo <diogobissolimoreno@gmail.com>
2021-06-09 23:28:57 -0300

    Iframe para página do Facebook adicionado
    > Classes CSS adicionadas

--- src/pages/Programacao/Programacao.jsx
+++ src/pages/Programacao/Programacao.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import BarraNavegacao from "./../../components/BarraNavegacao/BarraNavegacao";
 import Rodape from "./../../components/Rodape/Rodape";
+import Titulo from './../../components/Titulo/Titulo'
 import Container from "@material-ui/core/Container";
 import { TwitterTimelineEmbed } from "react-twitter-embed";
 import { Calendar } from "react-calendar";
@@ -50,36 +51,49 @@ class Programacao extends Component {
         sm="maxWidth"
       >
         <BarraNavegacao></BarraNavegacao>
+        <Titulo titulo={"PROGRAMAÇÃO"}></Titulo>
 
-        <Calendar
-          onClickDay={this.handleChange}
-          next2Label={null}
-          prev2Label={null}
-          showNeighboringMonth={false}
-          view={"mounth"}
-          
-          tileClassName={({ date })=> {
-            let classes = '';
-            if(date.getDay() === 0){
-              classes = "domingo ";
-            }
-            if (this.constroiEvento(date)){
-              classes += "evento"
-            }
+        <div id="calendario">
+          <Calendar
+            onClickDay={this.handleChange}
+            next2Label={null}
+            prev2Label={null}
+            showNeighboringMonth={false}
+            view={"mounth"}
+            tileClassName={({ date }) => {
+              let classes = "";
+              if (date.getDay() === 0) {
+                classes = "domingo ";
+              }
+              if (this.constroiEvento(date)) {
+                classes += "evento";
+              }
               return classes;
-          }
-          }
-        />
-        <div>
-          <p>Aqui é o painel de informações</p>
-          {this.constroiEvento(this.state.data)}
-        </div>
+            }}
+          />
+          <div>
+            <p>Aqui é o painel de informações</p>
+            {this.constroiEvento(this.state.data)}
+          </div>
 
-        <TwitterTimelineEmbed
-          sourceType="@Pontifex_pt"
-          screenName="Pontifex_pt"
-          options={{ height: 400, width: 400 }}
-        />
+        </div>
+        <div id="redesSociais">
+          <TwitterTimelineEmbed
+            sourceType="@Pontifex_pt"
+            screenName="Pontifex_pt"
+            options={{ height: 450, width: 400 }}
+          />
+          <iframe
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fdiocesedebauru&tabs=timeline&width=400&height=450&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+            width="400"
+            height="450"
+            style={{ border: "none" }}
+            scrolling="no"
+            frameborder="0"
+            allowfullscreen="true"
+            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+          ></iframe>
+        </div>
 
         <Rodape></Rodape>
       </Container>
