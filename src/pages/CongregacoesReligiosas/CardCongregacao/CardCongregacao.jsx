78ed266b45a541432e07e572a31b1a035b8fe1f2
Dibimo <diogobissolimoreno@gmail.com>
2021-06-10 14:52:11 -0300

    Estilo incrementado

--- src/pages/CongregacoesReligiosas/CardCongregacao/CardCongregacao.jsx
+++ src/pages/CongregacoesReligiosas/CardCongregacao/CardCongregacao.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Container from "@material-ui/core/Container";
 import './estiloCardCongregacao.css'
 class CardCongregacao extends Component {
   constructor(props){
@@ -13,7 +12,7 @@ class CardCongregacao extends Component {
     let titulos = [];
     congregacao.titulos.map((titulo)=>{
       titulos.push(
-        <div style={{color: "#8b0000"}}>
+        <div className="negrito" style={{ color: "#8b0000" }}>
           {titulo}
         </div>
       );
@@ -31,22 +30,22 @@ class CardCongregacao extends Component {
 
     return (
       <div className="conteudoCardCongregacao">
-        <div>{titulos}</div>
-        <div>{nomes}</div>
+        {titulos}
+        {nomes}
         <div>Cidade: {congregacao.cidade}</div>
         <div>Endereço: {congregacao.local}</div>
         <div>CEP: {congregacao.cep}</div>
-        <div>Telefone(s): {congregacao.telefone}</div>
-        <div>{(congregacao.email)&&("E-mail: "+congregacao.email)}</div>
-        {/*Caso não haja caixa postal registrada (null) essa div não terá conteudo*/}
         <div>{(congregacao.caixaPostal && ("Caixa postal: "+congregacao.caixaPostal))}</div>
+        <div className="negrito">Telefone(s): {congregacao.telefone}</div>
+        <div className="negrito">{(congregacao.email)&&("E-mail: "+congregacao.email)}</div>
+        {/*Caso não haja caixa postal registrada (null) essa div não terá conteudo*/}
       </div>
     );
   }
 
   render() {
     return (
-      <div style={{ border: "1px solid" }}>
+      <div className="conteinerCardCongregacao">
         {this.constroiCard(this.props.congregacao)}
       </div>
     );
