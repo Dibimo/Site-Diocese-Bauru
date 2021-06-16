bfb04cac9f67567f1d8328619ced13a26228b46b
Dibimo <diogobissolimoreno@gmail.com>
2021-06-16 14:17:13 -0300

    Rotas finalizadas

--- src/components/BarraNavegacao/BarraNavegacao.jsx
+++ src/components/BarraNavegacao/BarraNavegacao.jsx
@@ -11,28 +11,32 @@ const BarraNavegacao = (props) => {
   const toggleNavbar = () => setCollapsed(!collapsed);
 
     return (
-        <div style={{backgroundColor: "darkred"}}>
+        <div id="barraNavegacao" style={{backgroundColor: "darkred"}}>
             <div className="navbar-dark  rounded d-flex flex-column">
                 <div className="p-2">
                     <Navbar className=" rounded" light expand="row">
-                    <Link to="/"><Col><NavbarBrand className="d-flex flex-row float-left">Home</NavbarBrand></Col></Link>
-                    <Link to="/administracao"><Col><NavbarBrand className="d-flex flex-row float-left">Administração</NavbarBrand></Col></Link>
-                    <Link to="/paroquias"><Col><NavbarBrand href="/paroquia" className="d-flex flex-row float-left">Paróquias</NavbarBrand></Col></Link>
-                    <Link to="/contato"><Col><NavbarBrand className="d-flex flex-row float-left">Contato</NavbarBrand></Col></Link>
+                    <Link to="/"><Col><NavbarBrand className="d-flex flex-row float-left">HOME</NavbarBrand></Col></Link>
+                    <Link to="/administracao"><Col><NavbarBrand className="d-flex flex-row float-left">ADMINISTRAÇÃO</NavbarBrand></Col></Link>
+                    <Link to="/paroquias"><Col><NavbarBrand href="/paroquia" className="d-flex flex-row float-left">PARÓQUIAS</NavbarBrand></Col></Link>
+                    <Link to="/contato"><Col><NavbarBrand className="d-flex flex-row float-left">CONTATO</NavbarBrand></Col></Link>
                     <Col><NavItem href="/" className="d-flex flex-row float-rigth"><input type="text" placeholder="Pesquisar..."></input></NavItem></Col>
                     <NavbarToggler onClick={toggleNavbar} className="mr-2" />
                     <Collapse isOpen={!collapsed} navbar>
                         <Nav navbar>
                             <NavItem>
-                                <Link to="/congregacoes_religiosas" className="semDecoracao"><h4>Congregações Religiosas</h4></Link>
+                                <Link to="/congregacoes_religiosas" className="semDecoracao"><h4>CONGREGAÇÕES RELIGIOSAS</h4></Link>
                             </NavItem>
 
                             <NavItem>
-                                <Link to="/noticias" className="semDecoracao"><h4>Notícias Diocesanas</h4></Link>
+                                <Link to="/noticias" className="semDecoracao"><h4>VOZ DO PASTOR</h4></Link>
                             </NavItem>
 
                             <NavItem>
-                                <NavLink href="/contato"><h4>...</h4></NavLink>
+                              <Link to="/movimentos" className="semDecoracao"><h4>PASTORAIS E MOVIMENTOS</h4></Link>
+                            </NavItem>
+
+                            <NavItem>
+                              <Link to="/clero" className="semDecoracao"><h4>PADRES DA DIOCESE DE BAURU</h4></Link>
                             </NavItem>
                         </Nav>
                     </Collapse>
